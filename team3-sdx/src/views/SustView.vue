5614ee10c08ba54e7ce7e20096cc5854db5d7812
Silvia Götti <dev@silviagoetti.ch>
2023-11-12 16:58:19 +0100

    video demo

--- team3-sdx/src/views/SustView.vue
+++ team3-sdx/src/views/SustView.vue
@@ -1,10 +1,8 @@
 <template id="sust">
   <div class="sust">
-    <video controls>
-      <source src="./video.mp4" type="video/mp4">
 
-    </video>
 
+    <a href="./video.mp4"><button class="button-9">Click here for Demo</button></a>
   </div>
 </template>
 
@@ -15,5 +13,43 @@
     display: flex;
     align-items: center;
   }
+}
+
+/* CSS */
+.button-9 {
+  appearance: button;
+  backface-visibility: hidden;
+  background-color: #1c6aaf;
+  border-radius: 6px;
+  border-width: 0;
+  box-shadow: rgba(50, 50, 93, .1) 0 0 0 1px inset, rgba(50, 50, 93, .1) 0 2px 5px 0, rgba(0, 0, 0, .07) 0 1px 1px 0;
+  box-sizing: border-box;
+  color: #fff;
+  cursor: pointer;
+  font-family: -apple-system, system-ui, "Segoe UI", Roboto, "Helvetica Neue", Ubuntu, sans-serif;
+  font-size: 100%;
+  height: 50px;
+  line-height: 1.15;
+  margin: 12px 0 0;
+  outline: none;
+  overflow: hidden;
+  padding: 0 25px;
+  position: relative;
+  text-align: center;
+  text-transform: none;
+  transform: translateZ(0);
+  transition: all .2s, box-shadow .08s ease-in;
+  user-select: none;
+  -webkit-user-select: none;
+  touch-action: manipulation;
+  width: 100%;
+}
+
+.button-9:disabled {
+  cursor: default;
+}
+
+.button-9:focus {
+  box-shadow: rgba(50, 50, 93, .1) 0 0 0 1px inset, rgba(50, 50, 93, .2) 0 6px 15px 0, rgba(0, 0, 0, .1) 0 2px 2px 0, rgba(50, 151, 211, .3) 0 0 0 4px;
 }
 </style>
